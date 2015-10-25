b8ba557d045837d286f8112b00b527ce1ef27f57
monokit <micka@azulis.be>
2015-10-25 02:01:47 +0200

    Update index.php

--- Public/index.php
+++ Public/index.php
@@ -6,7 +6,7 @@
 
     $Application = new MonoKitApplication();
     $Application->AppRegistry()->setFromIniFile( "../Config/AppConfig.ini" );
-    $Application->AppRoute()->setFromIniFile( "../Config/AppRoute.ini" );
+    $Application->AppRouter()->setFromIniFile( "../Config/AppRoute.ini" );
     $Application->render( "index.view.php" );
 
 ?>
